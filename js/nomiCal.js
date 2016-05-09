5af1af1664c47e2a8d598f227863df3bb39ed747
Keisuke Nakama <nakamakeisuke@nakabook.local>
2016-05-10 00:03:45 +0900

    合計額の平均を算出

--- js/nomiCal.js
+++ js/nomiCal.js
@@ -6,29 +6,39 @@ MyApp.controller('nomiCalAppCtrl', ['$scope' ,function($scope){
 	var index = 0;
 
 	// var nomiCal = $scope;
-	$scope.member = [
-      {id:index, name:'中間'},
+	$scope.nomiMember = [
+      {id:index, name:'中間', price:'0'},
 	];
 
 	index++;
 
 	$scope.addMember = function(){
-		$scope.member.push({id:index, name:$scope.memberName});
-		$scope.addMemberName = '';
-		index++;
+		$scope.nomiMember.push({id:index, name:$scope.memberName,price:''});
+		$scope.memberName = '';
+		index++;	
 	};
 
-	$scope.deleteMember = function(delMember){
+	$scope.deleteMember = function(member){
 		var delIndex = 0;
 		var m;
-		for(var i=0; $scope.member.length; i++){
-			m = $scope.member[i];
-			if(m.id == delMember.id){
+		for(var i=0; $scope.nomiMember.length; i++){
+			m = $scope.nomiMember[i];
+			if(m.id == member.id){
 				delIndex = i;
 				break;
 			}
 		}
-		$scope.member.splice(delIndex, 1);
+		$scope.nomiMember.splice(delIndex, 1);
 	};
+
+	$scope.calcPrice = function(member){
+		var totalPrice = $scope.totalPrice;
+		var sum = $scope.nomiMember.length;
+
+		member.price = totalPrice/sum;
+		return member.price;
+	};
+
+
 
 }]);
