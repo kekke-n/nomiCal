82b67ab32ca43c7836620a79cdec1c7b18c6e51f
Keisuke Nakama <nakamakeisuke@nakabook.local>
2016-05-10 00:34:48 +0900

    コメント追加

--- js/nomiCal.js
+++ js/nomiCal.js
@@ -12,23 +12,26 @@ MyApp.controller('nomiCalAppCtrl', ['$scope' ,function($scope){
 
 	index++;
 
+	// のみ仲間追加関数
 	$scope.addMember = function(){
 		$scope.nomiMember.push({id:index, name:$scope.memberName,price:''});
 		$scope.memberName = '';
-		index++;	
+		index++;
 	};
 
+	// のみ仲間削除関数
 	$scope.deleteMember = function(member){
 		var delIndex = 0;
 		var m;
 		for(var i=0; $scope.nomiMember.length; i++){
 			m = $scope.nomiMember[i];
 			if(m.id == member.id){
+				// 削除対象のメンバーのidと一致した場合
 				delIndex = i;
 				break;
 			}
 		}
-		$scope.nomiMember.splice(delIndex, 1);
+		$scope.nomiMember.splice(delIndex, 1); // 一致したidをのメンバーを削除する
 	};
 
 	$scope.calcPrice = function(member){
