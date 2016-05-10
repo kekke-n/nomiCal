4c2f0d3a9fa91c2985661be0439a55c8189322be
Keisuke Nakama <nakamakeisuke@nakabook.local>
2016-05-11 01:05:58 +0900

    役職によって負担額が増加する機能を追加

--- js/nomiCal.js
+++ js/nomiCal.js
@@ -13,13 +13,26 @@ MyApp.controller('nomiCalAppCtrl', ['$scope' ,function($scope){
 	index++;
 
 	// 役職選択肢
-	$scope.positions = ['一般','部長','課長','チーフ'];
+	$scope.positions = ['一般', '部長', '課長', 'チーフ']; 
+	// $scope.positions = [
+	// 	{type:'一般', weight:'1'},
+	// 	{type:'部長', weight:'1.5'},
+	// 	{type:'課長', weight:'1.3'},
+	// 	{type:'チーフ', weight:'1.2'}
 	// デフォルト値の設定
-	$scope.position = $scope.positions[0];
+	// $scope.nomiMember.position = $scope.positions[0].type;
+
+	// 飲み代重みテーブル
+	var weightTable = [
+		{type:'一般', weight:1},
+		{type:'部長', weight:1.5},
+		{type:'課長', weight:1.3},
+		{type:'チーフ', weight:1.2}
+	];
 
 	// のみ仲間追加関数
 	$scope.addMember = function(){
-		$scope.nomiMember.push({id:index, name:$scope.memberName, position:'一般', weight:1, price:''});
+		$scope.nomiMember.push({id:index, name:$scope.memberName, position:'一般', weight:1, price:0});
 		$scope.memberName = '';
 		index++;
 	};
@@ -37,6 +50,15 @@ MyApp.controller('nomiCalAppCtrl', ['$scope' ,function($scope){
 			}
 		}
 		$scope.nomiMember.splice(delIndex, 1); // 一致したidをのメンバーを削除する
+	};
+
+	// 配列の合計値取得関数
+	function getSum(array){
+		var sum = 0;
+		for(var i=0; i < array.length; i++){
+			sum = sum + array[i];
+		}
+		return sum;
 	};
 
 	// 500円毎に切り上げする関数
@@ -62,12 +84,128 @@ MyApp.controller('nomiCalAppCtrl', ['$scope' ,function($scope){
 		}
 	};
 
-	$scope.calcPrice = function(member){
+	// 500円毎に切り下げる関数
+	function floatFiveHundred(num){
+
+		if((num % 1000) == 0){
+			// 1000の倍数である場合
+			return num;
+		}
+
+		var hundredDigit = Math.floor(num / 1000);
+		
+		if((num % 1000) > 500){
+			// 下３桁が500より上
+			return (hundredDigit * 1000) + 500;
+		}
+		else if((num % 1000) <= 500){
+			// 下３桁が500以下
+			return hundredDigit * 1000;
+		}
+		else{
+			return num;
+		}
+	};
+
+	// 飲み代重み計算関数
+	function calcWeight(){
+		var member = $scope.nomiMember;
+
+		for(var m in member){
+			for(var w in weightTable){
+				if(member[m].position == weightTable[w].type){
+					member[m].weight =weightTable[w].weight;
+					break;
+				}
+			}
+		}
+	};
+
+	// 重み掛け関数
+	function multiplyPriceByWeight(priceList){
+		var member = $scope.nomiMember;
+
+		for(var i = 0; i < priceList.length; i++){
+			priceList[i] = priceList[i]* member[i].weight;
+		}
+
+		return priceList;
+	}
+
+
+	// 余剰金分配計算関数
+	function paybackSurplus(priceList, surplus){
+		for(var i=0; i < priceList.length; i++){
+			priceList[i] = floatFiveHundred(priceList[i] - surplus);
+		}
+		return priceList;
+	}
+
+	// 上司の不足金負担額　計算関数
+	function addToBuss(priceList, shortage){
+		var member = $scope.nomiMember;
+		var maxWeight = member[0].weight;
+		var maxIndex = 0;
+
+		for(var i=0; i < priceList.length; i++){
+			if(i == 0) continue;
+			if(member[i].weight > maxWeight){
+				maxWeight = member[i].weight;
+				maxIndex = i;
+			}
+		}
+
+		// 役職が最も高い人に不足金を追加する
+		priceList[maxIndex] = priceList[maxIndex]  + shortage;
+
+		return priceList;
+	}
+
+	// 
+	function setAllMemeberPrice(priceList){
+		var member = $scope.nomiMember;
+		for(var i=0; i < priceList.length; i++){
+			member[i].price = priceList[i];
+		}
+	}
+
+	// 個人の飲み代計算関数
+	$scope.showPrice = function(member){
 		var totalPrice = $scope.totalPrice;
 		var sum = $scope.nomiMember.length;
+		var split = 0;
+		var priceList = [];
+		var surplus = 0;		// 余剰金
+		var shortage = 0;		// 不足金
 
-		member.price = ceilFiveHundred(totalPrice/sum);
+		calcWeight();
+
+		split  = ceilFiveHundred(totalPrice/sum);
+
+		for(var i = 0; i < sum; i++){
+			priceList[i] = split;
+		}
+
+		priceList = multiplyPriceByWeight(priceList);
+		if(getSum(priceList) > totalPrice){
+			// 支払額より多い場合
+			surplus = (getSum(priceList) - totalPrice) / sum;
+			priceList = paybackSurplus(priceList, surplus);
+		}
+		if(getSum(priceList) < totalPrice){
+			// 支払額より少ない場合
+			shortage = totalPrice - getSum(priceList);
+			priceList = addToBuss(priceList, shortage);
+		}
+		// member.price = multiplyPriceByWeight(member, tmpPrice);
+
+		setAllMemeberPrice(priceList);
+
 		return member.price;
 	};
+
+
+	// 初期処理
+	calcWeight();
 
 }]);
